745282899ea70468c99fb603b5cd89537ca24622
Arthur <arthurd800@wp.pl>
2019-12-14 18:25:33 +0100

    now collection items can be added to the cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,9 @@
 import {CartActionTypes} from './cart.types';
 
-const INITIAL_STATE = {hidden: true};
+const INITIAL_STATE = {
+    hidden: true,
+    cartItems: []
+};
 
 const cartReducer = (state = INITIAL_STATE,action)=>{
     switch(action.type)
@@ -10,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE,action)=>{
                 ...state,
                 hidden: !state.hidden
             };
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems: [...state.cartItems,action.payload]
+            }
         default:
             return state;
     }
