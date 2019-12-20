a63ebfaa8e5da802e3e49b00d11398281a3e5eaa
Arthur <arthurd800@wp.pl>
2019-12-20 14:13:51 +0100

    added shop directory to redux folder, moved collections list from the component to seperate file,added collection-overview and collection components, added new reducers

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import './collection.styles.sass';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import {selectCollection} from '../../redux/shop/shop.selectors';
+
+const CollectionPage = ({collection})=>{
+    console.log(collection);
+    return(
+        <div className="collection-page">
+            <h2>COLLECTION PAGE</h2>
+        </div>
+    );
+};
+
+const mapStateToProps = (state,ownProps)=>({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
